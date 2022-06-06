548dcb253ce9e343147aa344dc700580f1520746
SJ4t <sergei.juganson@gmail.com>
2022-06-06 18:06:58 +0300

    homework 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavComponent from './Header/NavComponent';
-import { Container, Row, Col from 'react-bootstrap'
+import { Container, Row, Col} from 'react-bootstrap';
 import from 'module';
- }
+
 function App() {
+
+  function handleOnSubmit (event) {
+    event.preventDeaful();
+    const city = event.target.city.value
+    const city = event.target.city.value
+
+    const city = event.target.city.value
+    const city = event.target.city.value
+  }
+
   return (
     <Container>
      <Row>

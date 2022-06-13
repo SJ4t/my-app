d9d28581e2133c5f7ece2f5258a56553236d7e52
SJ4t <sergei.juganson@gmail.com>
2022-06-13 17:40:47 +0300

    comment delited

--- src/App.js
+++ src/App.js
@@ -7,28 +7,15 @@ import dataTypes from './Header/type.json';
 import MapComponent from './Body/MapComponent';
 import { useCookies } from 'react-cookie';
 
-function App() {//главный компонент реакта, который рендерится в наш главный <div id=root>
+function App() {
 
   const [ form, setForm ] = useState(null);
-  //это хук который отвечает за состояниия компонента.
-  //Все хуки в реакте начинаются со словом 'use"
-  //useState возвращает массив с двумя элементами: первое - текущее значение...
-  //при обновлении состояния у нас происходит рендер Componets
-  //useState как многие хуки принимайут праоаметры - значение начальго состояния 
   const [cookies, setCookie, removeCookie] = useCookies(['weather']);
-//ето хук с дополнительного пакета npm react-cookie
-//useСoockie massiwe c y
-
 
   console.log(cookies.weather);
-  // для выведения информации в console developer tools
 
-  // обработчик submit события
   function handleOnSubmit(event) {
-    // запрещаем ему действовать по умолчанию
     event.preventDefault();
-    // получаем информацию от пользователя. то есть то, что он вписал в input
-    // event.target.city.value = {событие}.{данныеDOM}.{имя input тэга}. {его значение}
     const city = event.target.city.value;
     const unit = event.target.unit.value;
     let selectedTypes = [];
@@ -38,10 +25,7 @@ function App() {//главный компонент реакта, который
         selectedTypes.push(dtype.value);
       }
     }
-// filter как forEach и map и for проходит по всему массиву [] но оставляет только те назначения, которые нам нужны
-// и он вернёт массиве уже с нужным нам данными
 
-// .includes просто смотрит на массив и ищет есть ли в нём значения полученное с аргумента возвращает true иши false
     let excludeDataType = dataTypes.filter(dtype => !selectedTypes.includes(dtype.value));
     const language = event.target.language.value;
     const updateData = {
@@ -50,18 +34,9 @@ function App() {//главный компонент реакта, который
       language,
       excludeDataType,
     };
-    // setForm - это функция 
     setForm(updateData);
-    // создаёт задаёт куки
     setCookie('weather', updateData);
   }
-
-//Component долежн всегда возвращать реакт елемент JSX
-//JSX: Javascript с хтмл - который нам помогает совместить их вместе
-//Script который совмещает их вместе
-//JSX требует root element. у него должен бытьь один главный елемент. хтмл
-//длля указиния 
-//Properties передаются в компонент через html 
 
   return (
     <Container>
